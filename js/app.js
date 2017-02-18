25c2f22ee5cd8a8044f6d45ce83285489a1e0424
Christine <Christine@Christines-MacBook-Pro.local>
2017-02-17 21:52:13 -1000

    DOMS Exercise

--- js/app.js
+++ js/app.js
@@ -54,3 +54,12 @@ aliasBox.appendChild(divElement);
 
 //Final Boss
 /*8. Create your own profile.*/
+function myCreateFunction(){
+	var table = document.getElementById("myTable");
+	var row = table.insertRow(0);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	cell1.innerHTML = "Christine";
+
+}
+
